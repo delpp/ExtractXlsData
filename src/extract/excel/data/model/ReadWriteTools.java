fce716867a08a0416d8cf17148c1b8e239efef81
delpp <delpp@wp.pl>
2021-10-08 15:39:53 +0200

    Add files via upload

---
+++ src/extract/excel/data/model/ReadWriteTools.java
@@ -0,0 +1,16 @@
+package extract.excel.data.model;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class ReadWriteTools {
+	public static void saveFile(String fileName, String text){
+		try (PrintStream out = new PrintStream(new FileOutputStream(fileName))) {
+		    out.print(text);
+		}
+		catch (IOException ex){
+			ex.printStackTrace();
+		};
+	}
+}
